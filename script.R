2213a7ffa796b09aaa586c9039adc0f8dab32c4e
StockierAsp9634 <arnav.mahendra07@gmail.com>
2025-02-23 16:34:39 -0800

    Commit #2

--- script.R
+++ script.R
@@ -5,6 +5,10 @@ library(lubridate)
 
 file_path <- "C:/Users/arnav/OneDrive/Documents/CAenviroanalysis/data/ghg_inventory_by_ipcc_all_00-21.xlsx"
 includedemissions <- read_excel(file_path, sheet = 2)
-excludedemissions <- read_excel(file_path, sheet = 3)
 
 summary(includedemissions)
+
+emissionslevelZero <- read_csv("C:/Users/arnav/Documents/CAenviroanalysis/data/levelZero.csv")
+
+summary(emissionslevelZero)
+
