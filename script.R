ab53abab56ffcd49d1e7035ff3721889160873a4
StockierAsp9634 <73668708+StockierAsp9634@users.noreply.github.com>
2025-02-26 12:52:57 -0800

    More plots

--- script.R
+++ script.R
@@ -11,6 +11,9 @@ summary(includedemissions)
 
 emissionslevelZero <- read_csv("C:/Users/arnav/Documents/CAenviroanalysis/data/levelZero.csv")
 
+emissionslevelZero <- emissionslevelZero |> rename(totalIncludedEmissions = `Total Included Emissions`) 
+
+
 summary(emissionslevelZero)
 
 emissionslevelZeroprelaw <- filter(emissionslevelZero, Year < 2013)
@@ -18,10 +21,12 @@ emissionslevelZeroprelaw <- filter(emissionslevelZero, Year < 2013)
 emissionslevelZeropostlaw <- filter(emissionslevelZero, Year >= 2013)
 
 
-emissionslevelZero <- emissionslevelZero |> rename(totalIncludedEmissions = `Total Included Emissions`) 
 
-ggplot(data = emissionslevelZero, aes(x = Year, y = `Total Included Emissions`)) + geom_line() 
+ggplot(data = emissionslevelZero, aes(x = Year, y = totalIncludedEmissions)) + geom_line() + ylab("Total Included Emissions") + labs(title = "California GHG emissions over the years")
 
 
+ggplot(data = emissionslevelZeroprelaw, aes(x = Year, y = `Total Included Emissions`)) + geom_line()
+
+ggplot(data = emissionslevelZeropostlaw, aes(x = Year, y = `Total Included Emissions`)) + geom_line()
 
 
