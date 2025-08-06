7b723d4065248530ff2fd67e38e9460f780a1a75
tydiq7 <aravkanyal@gmail.com>
2025-08-06 14:06:55 -0700

    Data Wrangling

--- script.R
+++ script.R
@@ -29,4 +29,22 @@ ggplot(data = emissionslevelZeroprelaw, aes(x = Year, y = `Total Included Emissi
 
 ggplot(data = emissionslevelZeropostlaw, aes(x = Year, y = `Total Included Emissions`)) + geom_line()
 
+#importing data set
+emData <- read.csv("C:\\Users\\Arav Kanyal\\Documents\\CAenviroanalysis\\data\\nc-ghg_inventory_sector_all_00-22(Included emissions).csv")
 
+#wrangling
+colnames(emData) <- emData[1, ]
+emData <- emData[-1, ]
+emData <- emData [, -c(1:2, 4:8, 34)]
+emData <- na.omit(emData)
+emData$`Sector Level 1` <- as.factor(emData$`Sector Level 1`)
+emData <- emData %>%
+  group_by(`Sector Level 1`) %>%
+  summarise(across(3:25, sum))
+emData <- t(emData)
+emData <- as.data.frame(emData)
+colnames(emData) <- emData[1, ]
+emData <- emData[-1, ]
+
+#Emissions by Sector (2000-22)
+plot.ts(emData)
